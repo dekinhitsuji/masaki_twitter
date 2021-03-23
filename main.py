e10809182988a04a5daedf0be05ba866c73d2290
dekinhitsuji <yattusyuiti@gmail.com>
2021-03-23 23:28:31 +0900

    change acount

---
+++ main.py
@@ -0,0 +1,98 @@
+import tweepy
+import time
+import random
+import configparser
+import schedule
+
+conf = configparser.ConfigParser()
+conf.read('config.ini')
+
+CONSUMER_KEY = conf['twitter']['API_KEY']
+CONSUMER_SECRET = conf['twitter']['API_SECRET']
+ACCESS_TOKEN = conf['twitter']['ACCESS_TOKEN']
+ACCESS_SECERET = conf['twitter']['ACCESS_TOKEN_SECRET']
+
+
+# consumer　第一引数に(consumer　key)　第二引数に(consumer　secret) #
+auth = tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
+# ACCESS_TOKEN_KEY 第一引数に(Access token)　第二引数に(Access token secret) #
+auth.set_access_token(ACCESS_TOKEN,ACCESS_SECERET)
+
+# wait_on_rate_limit = レート制限が補充されるのを自動的に待つかどうか #
+# wait_on_rate_limit_notify = Tweepyがレート制限の補充を待っているときに通知を出力するかどうか #
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+
+# screen name (@name)を取得
+my_name = api.me()
+my_screen_name = my_name.screen_name
+
+
+# 取得したいキーワード #
+list = ['#裏アカ男子','#裏アカ女子と繋がりたい','#チン凸','#巨根','#彼女募集中','#見せ合い','#見る専','#まん凸','#裏垢女子と繋がりたい','#裏垢男子']
+reserve = ['#裏アカ男子','#裏アカ女子と繋がりたい','#チン凸','#巨根','#彼女募集中','#見せ合い','#見る専','#まん凸','#裏垢女子と繋がりたい','#裏垢男子']
+
+# ツイート数10件 #
+tweet_count = 50
+
+#自動フォローと判定されにくいようにフォロー間隔 sleep をダンダム値とする
+random_min_time = 4 #sleepでとり得る最小値
+random_max_time = 15 #sleepでとり得る最大値
+
+
+
+def main():
+    global list
+    global reserve
+    if list == 0:
+        list = reserve
+    search_list = random.choice(list)
+    search = search_list
+
+    liked_count = 0 #いいねカウンター
+
+    print(my_screen_name,': ','------------------------------')
+    print(my_screen_name,': ','Searching... {}' .format(search))
+    # サーチ結果を格納 #
+    search_result = api.search(q=search, count=tweet_count)
+
+    for tweet in search_result:
+        tweet_id = tweet.id
+        user_name = tweet.user._json['name']
+        user_screen_name = tweet.user._json['screen_name']
+        tweet_status = api.get_status(tweet_id)
+        tweet_favorite_status = tweet_status.favorited
+
+        check_status = 'False'
+        tweet_favorite_status_01 = str(tweet_favorite_status)
+
+
+        if tweet_favorite_status_01 == check_status:
+            if liked_count == 1:
+                continue
+            try:
+                api.create_favorite(id=tweet_id)   # いいねの処理
+            except tweepy.TweepError as e:
+                print(e.reason)
+            except StopIteration:
+                continue
+            tweet_status = api.get_status(tweet_id)
+            tweet_favorite_status = tweet_status.favorited
+            print(my_screen_name,': ',user_name,'(',user_screen_name,")","いいねしました(STATUS =",tweet_favorite_status,")")
+            liked_count += 1
+            sleep_random_time = random.randint(random_min_time,random_max_time)
+            print(my_screen_name,': ',' 次のいいねまで' ,sleep_random_time,'秒待ちます')
+            time.sleep(sleep_random_time)
+        else:
+            print(my_screen_name,': ',user_name,'(',user_screen_name,')','はいいね済み(',tweet_favorite_status,')なのでスキップしました')
+
+    print(my_screen_name,": ","search =",search,"で",liked_count,"人いいねしました。終了します。")
+
+
+schedule.every(10).minutes.do(main)
+
+
+
+while True:
+  schedule.run_pending()
+  time.sleep(60)
